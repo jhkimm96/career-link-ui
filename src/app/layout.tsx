f169fd3b1ba1a01d9a194856e43f7f47595a48d1
JIHEE <jhkimm96@gmail.com>
2025-07-23 02:31:42 +0900

    feat : 화면전환시 최상단 스크롤 및 상단 스크롤 버튼 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { ReactNode } from 'react';
+import ScrollToTopRoute from '@/components/layouts/scrollToTopRoute';
+import ScrollToTopButton from '@/components/layouts/scrollToTopButton';
 
 export const metadata: Metadata = {
   title: 'Career Link',
@@ -10,7 +12,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body>
+        <ScrollToTopRoute />
+        {children}
+        <ScrollToTopButton />
+      </body>
     </html>
   );
 }
