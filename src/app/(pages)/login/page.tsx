14ac2b7ffac2a3e7457160e38c13f32d6a22ddbf
hayun0502 <hayun.dev00@gmail.com>
2025-08-01 17:22:10 +0900

    fix : 로그인 페이지 레이아웃 수정

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -7,14 +7,19 @@ import {
   Button,
   TextField,
   Typography,
-  Paper,
+  Stack,
   InputAdornment,
   IconButton,
+  Divider,
 } from '@mui/material';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import api from '@/api/axios';
 import { useAuth } from '@/libs/authContext';
+import PagesSectionLayout from '@/components/layouts/pagesSectionLayout';
+import Image from 'next/image';
+import GoogleIconButton from '@/components/googleLoginIcon';
+import KakaoIconButton from '@/components/kakaoLoginIcon';
 
 interface LoginResponse {
   accessToken: string;
@@ -54,32 +59,31 @@ const LoginPage: React.FC = () => {
   };
 
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      height="100vh"
-      bgcolor="#f5f5f5"
-    >
-      <Paper elevation={3} sx={{ padding: 4, width: 400 }}>
-        <Typography variant="h5" gutterBottom>
-          로그인
-        </Typography>
-
+    <PagesSectionLayout>
+      <Box
+        // component="form"
+        sx={{
+          p: 15,
+          border: '1px solid',
+          borderColor: 'divider',
+          borderRadius: 1,
+          rowGap: 2,
+        }}
+      >
         <TextField
           label="아이디"
-          fullWidth
-          margin="normal"
           value={loginId}
+          fullWidth
+          sx={{ marginBottom: 2 }}
           onChange={e => setLoginId(e.target.value)}
         />
 
         <TextField
           label="비밀번호"
-          fullWidth
           type={showPassword ? 'text' : 'password'}
-          margin="normal"
           value={password}
+          fullWidth
+          sx={{ marginBottom: 1 }}
           onChange={e => setPassword(e.target.value)}
           InputProps={{
             endAdornment: (
@@ -93,22 +97,55 @@ const LoginPage: React.FC = () => {
         />
 
         {errorMsg && (
-          <Typography variant="body2" color="error" mt={1}>
+          <Typography variant="body2" color="error" sx={{ marginBottom: 3 }} mt={1}>
             {errorMsg}
           </Typography>
         )}
 
-        <Button
-          variant="contained"
-          color="primary"
-          fullWidth
-          sx={{ marginTop: 3 }}
-          onClick={handleSubmit}
-        >
-          로그인
-        </Button>
-      </Paper>
-    </Box>
+        <Box display="flex" gap={2} marginBottom={2}>
+          <Button
+            variant="contained"
+            color="primary"
+            fullWidth
+            sx={{ flex: 1 }}
+            onClick={handleSubmit}
+          >
+            로그인
+          </Button>
+          <Button
+            variant="outlined"
+            color="primary"
+            fullWidth
+            sx={{ flex: 1 }}
+            onClick={() => {
+              router.push('/signup');
+            }}
+          >
+            회원가입
+          </Button>
+        </Box>
+        <Stack direction="row" spacing={1} justifyContent="flex-end">
+          <Button size={'small'}>아이디찾기</Button>
+          <Button size={'small'} href="#text-buttons">
+            비밀번호찾기
+          </Button>
+        </Stack>
+        <Divider>
+          <Typography variant="body2" color="text.secondary">
+            간편로그인
+          </Typography>
+        </Divider>
+        <Box display="flex" justifyContent="center" mt={2}>
+          <Stack direction="row" spacing={2} alignItems="center">
+            <KakaoIconButton />
+            <GoogleIconButton />
+          </Stack>
+        </Box>
+      </Box>
+      <Box display="flex" justifyContent="center" alignItems="center" flex={1} p={2}>
+        <Image src="/cardImg.png" alt="logo" width={400} height={200} />
+      </Box>
+    </PagesSectionLayout>
   );
 };
 
