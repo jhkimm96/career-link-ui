2c8d5d9de4a417fe910a321f8070c1d44db064db
JIHEE <jhkimm96@gmail.com>
2025-09-11 12:16:56 +0900

    feat : 기업/지원자 마이홈 추가

---
+++ src/app/(mypage)/mypage/emp/home/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { Tabs, Typography, Container, Tab } from '@mui/material';
+
+export default function MyPage() {
+  return (
+    <Container maxWidth="lg">
+      <Typography variant="h4" fontWeight={700} mb={3}>
+        기업
+      </Typography>
+      <Tabs value={0} textColor="primary" indicatorColor="primary">
+        <Tab label="공고별 지원자 수" />
+        <Tab label="지원상태별 지원자 수" />
+      </Tabs>
+
+      <Typography mt={4}>마이페이지에 오신 것을 환영합니다!</Typography>
+    </Container>
+  );
+}
---
+++ src/app/(mypage)/mypage/user/home/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { Tabs, Typography, Container, Tab } from '@mui/material';
+
+export default function MyPage() {
+  return (
+    <Container maxWidth="lg">
+      <Typography variant="h4" fontWeight={700} mb={3}>
+        지원자
+      </Typography>
+      <Tabs value={0} textColor="primary" indicatorColor="primary">
+        <Tab label="프로필" />
+        <Tab label="선호 채용분야에 따른 TOP10 공고 목록" />
+      </Tabs>
+
+      <Typography mt={4}>마이페이지에 오신 것을 환영합니다!</Typography>
+    </Container>
+  );
+}
